fd320ae529fac1d961639fc49d5b5773a14f2e76
Eslashier <anfelov@gmail.com>
2022-07-19 18:50:16 -0500

    Validations added in the DTOs

--- src/main/java/com/example/back/dto/CyclistDTO.java
+++ src/main/java/com/example/back/dto/CyclistDTO.java
@@ -4,15 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CyclistDTO {
 
     private String id;
+    @NotNull(message = "Name is mandatory")
     private String fullName;
+    @Positive
+    @NotNull(message = "Competition number is mandatory")
     private Integer competitionNumber;
+    @NotNull(message = "Team is mandatory")
     private String team;
+    @NotNull(message = "Nationality is mandatory")
     private String nationality;
 
 }
--- src/main/java/com/example/back/dto/TeamDTO.java
+++ src/main/java/com/example/back/dto/TeamDTO.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -13,8 +15,15 @@ import java.util.List;
 public class TeamDTO {
 
     private String id;
+
+    @NotNull(message = "Team name is mandatory")
     private String teamName;
+    @NotNull(message = "Team Abbreviation is mandatory")
+    @Size(min = 3, message = "{validation.teamAbbreviation.size.too_short}")
+    @Size(max = 3, message = "{validation.teamAbbreviation.size.too_long}")
     private String teamAbbreviation;
+    @NotNull(message = "Country is mandatory")
     private String country;
+    @Size(max = 8)
     private List<Cyclist> cyclists;
 }

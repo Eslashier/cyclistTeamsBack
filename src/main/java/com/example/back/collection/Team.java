addbf4cdad5ea7a290080c44d04526ef5ff1acc2
Eslashier <anfelov@gmail.com>
2022-07-21 10:44:16 -0500

    removed @Documentreference from team class

--- src/main/java/com/example/back/collection/Team.java
+++ src/main/java/com/example/back/collection/Team.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
@@ -23,6 +22,5 @@ public class Team {
     @Indexed(unique = true)
     private String teamAbbreviation;
     private String country;
-    @DocumentReference
     private List<Cyclist> cyclists;
 }
